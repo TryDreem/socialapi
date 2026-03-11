f23f53380a0c1a7c58ec463e47d4fa3e84344c73
Vlad Akulov <trydreem@MacBook-Pro-Vlad.local>
2026-03-11 09:58:54 +0100

    k6 test added

---
+++ load_test.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import {check, sleep} from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '30s', target: 50 },
+        { duration: '1m', target: 50 },
+        { duration: '30s', target: 100 },
+        { duration: '30s', target: 0 },
+    ],
+}
+
+export default function () {
+    const res = http.get('http://localhost/post');
+
+    check(res, {
+        'status is 200': (r) => r.status ===200,
+        'response time < 500ms': (r) => r.timing.duration < 500,
+    });
+
+    sleep(1);
+}
